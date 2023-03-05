c8a7703bd9065e62efc54ab55d8e5ecc41e93cc4
abunbux <abunbux@gmail.com>
2023-03-05 22:56:15 +0300

    Добавление переключателя тёмной темы.

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// const btnDarkMode = document.querySelector(".dark-mode-btn");
+const btnDarkMode = document.querySelector(".dark-mode-btn");
+
+// btnDarkMode.onclick = function () {
+//     btnDarkMode.classList.toggle(".dark-mode-btn--active");
+// }
+
+btnDarkMode.onclick = function () {
+    btnDarkMode.classList.toggle("dark-mode-btn--active");
+}
