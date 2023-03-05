766836c03997e7fe55461a4c2afaae0ed54977ac
abunbux <abunbux@gmail.com>
2023-03-05 23:51:51 +0300

    Настройка тёмной темы.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
-// const btnDarkMode = document.querySelector(".dark-mode-btn");
 const btnDarkMode = document.querySelector(".dark-mode-btn");
-
-// btnDarkMode.onclick = function () {
-//     btnDarkMode.classList.toggle(".dark-mode-btn--active");
-// }
 
 btnDarkMode.onclick = function () {
     btnDarkMode.classList.toggle("dark-mode-btn--active");
+    document.body.classList.toggle("dark");
 }
